ca65eb184a214aee5151ec058a045f99e513a09b
frodeto <frodeto@hotmail.com>
2021-07-21 10:44:26 +0200

    from spring initializr

---
+++ src/main/java/org/frodetorvund/imgrec/ImgrecApplication.java
@@ -0,0 +1,13 @@
+package org.frodetorvund.imgrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgrecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgrecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/frodetorvund/imgrec/ImgrecApplicationTests.java
@@ -0,0 +1,13 @@
+package org.frodetorvund.imgrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgrecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
